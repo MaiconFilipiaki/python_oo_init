c297fdbe932c7b2290644f5d22b250df7c583286
Maicon Douglas Filipiaki <mdfilipiaki@gmail.com>
2019-11-14 15:16:42 -0300

    :construction: Multiple inheritance

--- inheritanceAndEncapsulation/modelo.py
+++ inheritanceAndEncapsulation/modelo.py
@@ -53,8 +53,7 @@ class Playlist:
     def listagem(self):
         return self._programas
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
 
 
@@ -75,7 +74,7 @@ filmes_e_series = [vingadores, got, demolidor, tmep]
 
 playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
 
-# print(len(playlist_fim_de_semana))
+print(len(playlist_fim_de_semana))
 
 for programa in playlist_fim_de_semana:
     print(programa)
